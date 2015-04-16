bd6395b4ed85ebf95df7dc1b88d3085b42810f4a
yfwang <yufei930101@gmail.com>
2015-04-16 15:24:05 +0800

    add model itemTest

---
+++ test/spec/model/itemTest.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var Item = require('../../../model/item');
+
+describe('item', function() {
+
+  var newItem = new Item;
+
+  beforeEach(function(done) {
+
+    Item.findById('5523cea79294d58a8e06c3bf')
+      .exec(function(err, item) {
+        newItem = item;
+        done();
+      });
+  });
+
+  it('should return right item id', function() {
+    var id = newItem.getId(newItem).toString();
+    expect(id).to.equal('5523cea79294d58a8e06c3bf');
+  })
+
+});
