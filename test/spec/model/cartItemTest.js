b960db3684d0eda001dff93fc9129e714bbeef0f
hawang <654296217@qq.com>
2015-04-16 15:32:25 +0800

    format

--- test/spec/model/cartItemTest.js
+++ test/spec/model/cartItemTest.js
@@ -2,31 +2,32 @@
 
 var CartItem = require('../../../model/cartItem');
 
-describe('cartItem model', function(){
+describe('cartItem model', function () {
 
   var cartItem;
-  beforeEach(function(done){
+
+  beforeEach(function (done) {
 
     CartItem.findById('551cc20e47a654d14a280e9c')
       .populate('item')
-      .exec(function(err, currentCartItem){
+      .exec(function (err, currentCartItem) {
 
         cartItem = currentCartItem;
         done();
       });
   });
 
-  afterEach(function(){
+  afterEach(function () {
 
     reloadDatabase();
   });
 
-  it('should get the subtotal of the cartItem', function(){
+  it('should get the subtotal of the cartItem', function () {
 
     expect(cartItem.getSubtotal()).to.equal('218.00');
   });
 
-  it('should get the itemId of the cartItem', function(){
+  it('should get the itemId of the cartItem', function () {
 
     expect(cartItem.getItemId(cartItem).toString()).to.equal('5523cea79294d58a8e06c3c0');
   });
--- test/spec/model/cartTest.js
+++ test/spec/model/cartTest.js
@@ -3,28 +3,28 @@
 var Cart = require('../../../model/cart');
 var CartItem = require('../../../model/cartItem');
 
-describe('cart model', function(){
+describe('cart model', function () {
 
   var cart = new Cart();
   var cartItems;
 
-  beforeEach(function(done){
+  beforeEach(function (done) {
 
     CartItem.findById('551cc20e47a654d14a280e9b')
       .populate('item')
-      .exec(function(err, cartItem){
+      .exec(function (err, cartItem) {
 
-      cartItems = [cartItem];
-      done();
-    });
+        cartItems = [cartItem];
+        done();
+      });
   });
 
-  afterEach(function(){
+  afterEach(function () {
 
     reloadDatabase();
   });
 
-  it('should get total of cartItems', function(){
+  it('should get total of cartItems', function () {
     expect(cart.getTotal(cartItems)).to.equal(120);
   });
 });
