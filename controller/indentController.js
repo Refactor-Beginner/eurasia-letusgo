7c4c483f0988008ad17f4e2897cb6b325b2c1d48
yfwang <yufei930101@gmail.com>
2015-04-16 17:14:58 +0800

    move method to indentController

--- controller/indentController.js
+++ controller/indentController.js
@@ -2,6 +2,22 @@
 
 var Indent = require('../model/indent');
 var Item = require('../model/item');
+var FormatUtil = require('../util/formatUtil.js');
+
+var NAME_LENGTH = 16;
+
+function getShortedCartItemName(cartItems) {
+
+  var shortedCartItemName = '';
+
+  cartItems.forEach(function(cartItem) {
+    if(cartItem.number > cartItem.item.inventory) {
+      shortedCartItemName += cartItem.item.name + '、';
+    }
+  });
+
+  return shortedCartItemName.substring(0, shortedCartItemName.length - 1);
+}
 
 var getIndent = function(req, res) {
 
@@ -21,6 +37,37 @@ var getIndent = function(req, res) {
     });
 };
 
+var renderIndentPage = function(req, res) {
+
+  Indent.findById('551fd16975cd55ed0cfa5503')
+    .populate('cartItems')
+    .exec(function(err, indent) {
+
+      Item.populate(indent, 'cartItems.item', function(err) {
+        if(err) {
+          throw err;
+        }
+
+        indent.cartItems.forEach(function(cartItem) {
+          cartItem.item.shortName = FormatUtil.parseString(cartItem.item.name, NAME_LENGTH);
+        });
+
+        var total = indent.getTotal(indent.cartItems);
+        var shortedCartItemName = getShortedCartItemName(indent.cartItems);
+
+        res.render('indent', {
+          cartItems: indent.cartItems,
+          total: total,
+          indent: indent,
+          shortedCartItemName: shortedCartItemName
+        });
+      });
+    });
+
+};
+
+
 module.exports = {
-  getIndent: getIndent
+  getIndent: getIndent,
+  renderIndentPage: renderIndentPage
 };
