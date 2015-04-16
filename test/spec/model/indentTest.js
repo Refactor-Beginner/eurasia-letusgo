2241e243166ea8f09d21d17610a3cd10f812427d
yfwang <yufei930101@gmail.com>
2015-04-16 16:50:42 +0800

    add indentTest.js

--- test/spec/model/indentTest.js
+++ test/spec/model/indentTest.js
@@ -1,3 +1,29 @@
-/**
- * Created by yfwang on 4/16/15.
- */
+'use strict';
+
+var Indent = require('../../../model/indent');
+var Item = require('../../../model/item');
+
+describe('indentModel', function() {
+
+  var indent = new Indent();
+  var cartItems;
+
+  beforeEach(function(done) {
+
+    Indent.findById('551fd16975cd55ed0cfa5503')
+      .populate('cartItems')
+      .exec(function(err, newIndent) {
+
+        indent = newIndent;
+        Item.populate(newIndent, 'cartItems.item', function() {
+          cartItems = newIndent.cartItems;
+          done();
+        });
+      });
+  });
+
+  it('should get right total', function() {
+        var total = indent.getTotal(cartItems);
+    expect(total).to.equal('3334.50');
+  });
+});
