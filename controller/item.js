314a956d3fb6b7ce72f9efd4c26752ffc06f8b54
yfwang <yufei930101@gmail.com>
2015-04-20 14:15:47 +0800

    update item controller

--- controller/item.js
+++ controller/item.js
@@ -1,6 +1,21 @@
 'use strict';
 var Item = require('../model/item');
 var Category = require('../model/category');
+var CartItem = require('../model/cartItem');
+
+function getItemsByCartItemId(cartItems, callback) {
+
+  var itemIds = [];
+  cartItems.forEach(function(cartItem) {
+
+    var cartItemModel = new CartItem();
+    itemIds.push(cartItemModel.getItemId(cartItem));
+  });
+
+  Item.where('_id').in(itemIds).exec(function(err, items) {
+    callback(items);
+  });
+}
 
 var renderItemDetail = function(req, res) {
   var id = req.params.id;
@@ -27,6 +42,63 @@ var renderItemDetail = function(req, res) {
     });
 };
 
+//var getItems = function(req, res) {
+//
+//  var cartItems = req.query.cartItems;
+//
+//  if(cartItems) {
+//
+//    getItemsByCartItemId(cartItems, function(items) {
+//      res.send(items);
+//    });
+//  } else {
+//
+//    Category.findById('551aa95e2ef086a169628b74')
+//      .populate('parent')
+//      .exec(function(err, category) {
+//
+//        Item.findById('551aac132ef086a169628b75')
+//          .populate('category')
+//          .exec(function(err, item) {
+//
+//            var test = {
+//              item: item,
+//              category: category
+//            };
+//
+//            res.send(test);
+//          });
+//      });
+//  }
+//};
+
+var getItemById = function(req, res) {
+
+  var id = req.params.id;
+
+  Item.findById(id, function(err, item) {
+
+    res.send(item);
+  });
+};
+
+var updateItem = function(req, res) {
+  var id = req.params.id;
+
+  var inventory = req.body.inventory;
+
+  Item.update({_id: id}, {
+    $set: {
+      inventory: inventory
+    }
+  }, function() {
+    res.send('inventory decrease successful');
+  });
+};
+
 module.exports = {
-  renderItemDetail: renderItemDetail
+  renderItemDetail: renderItemDetail,
+  //getItems: getItems,
+  getItemById: getItemById,
+  updateItem: updateItem
 };
--- router/route/item.js
+++ router/route/item.js
@@ -5,76 +5,8 @@ var router = express.Router();
 
 var itemController = require('../../controller/item.js');
 
-var Item = require('../../model/item');
-var CartItem = require('../../model/cartItem');
-var Category = require('../../model/category');
-
-function getItemsByCartItemId(cartItems, callback) {
-
-  var itemIds = [];
-  cartItems.forEach(function(cartItem) {
-
-    var cartItemModel = new CartItem();
-    itemIds.push(cartItemModel.getItemId(cartItem));
-  });
-
-  Item.where('_id').in(itemIds).exec(function(err, items) {
-    callback(items);
-  });
-}
-
-router.get('/', function(req, res) {
-
-  var cartItems = req.query.cartItems;
-
-  if(cartItems) {
-
-    getItemsByCartItemId(cartItems, function(items) {
-      res.send(items);
-    });
-  } else {
-
-    Category.findById('551aa95e2ef086a169628b74')
-      .populate('parent')
-      .exec(function(err, category) {
-
-        Item.findById('551aac132ef086a169628b75')
-          .populate('category')
-          .exec(function(err, item) {
-
-            var test = {
-              item: item,
-              category: category
-            };
-
-            res.send(test);
-          });
-      });
-  }
-});
-
-router.get('/:id', function(req, res) {
-  var id = req.params.id;
-
-  Item.findById(id, function(err, item) {
-
-    res.send(item);
-  });
-});
-
-router.post('/:id', function(req, res) {
-
-  var id = req.params.id;
-
-  var inventory = req.body.inventory;
-
-  Item.update({_id: id}, {
-    $set: {
-      inventory: inventory
-    }
-  }, function() {
-    res.send('inventory decrease successful');
-  });
-});
+//router.get('/', itemController.getItems);
+router.get('/:id', itemController.getItemById);
+router.post('/:id', itemController.updateItem);
 
 module.exports = router;
