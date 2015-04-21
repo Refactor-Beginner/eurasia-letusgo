ef0512e856dd236fb202def16fbbbf9bfe090ee8
yfwang <yufei930101@gmail.com>
2015-04-21 13:56:48 +0800

    add err test

--- test/spec/controller/itemTest.js
+++ test/spec/controller/itemTest.js
@@ -12,73 +12,91 @@ describe('GET /', function() {
     reloadDatabase();
   });
 
-  it('it should return itemDetails', function(done) {
+  describe('render item detail', function() {
+    it('it should return itemDetails', function(done) {
 
-    reqMock.params = {id: '5523cea79294d58a8e06c3bf'};
+      reqMock.params = {id: '5523cea79294d58a8e06c3bf'};
 
-    resMock.render = function(view, object) {
-      expect(view).is.to.equal('itemDetails');
-      expect(object).to.have.property('itemDetails');
-      expect(object.itemDetails.item.name).to.equal('水溶C100功能饮料');
+      resMock.render = function(view, object) {
+        expect(view).is.to.equal('itemDetails');
+        expect(object).to.have.property('itemDetails');
+        expect(object.itemDetails.item.name).to.equal('水溶C100功能饮料');
 
-      done();
-    };
+        done();
+      };
 
-    itemController.renderItemDetail(reqMock, resMock, next);
+      itemController.renderItemDetail(reqMock, resMock, next);
+    });
+
+    it('should err', function() {
+      var next = sinon.spy();
+      reqMock.params = {id: '11111'};
+
+      resMock.render = function() {
+        expect(next).calledOnce;
+      };
+      itemController.renderItemDetail(reqMock, resMock, next);
+    });
   });
 
-  it('it should get correct item by id', function(done) {
+  describe('send item', function() {
+    it('it should get correct item by id', function(done) {
 
-    reqMock.params = {id: '5523cea79294d58a8e06c3bf'};
+      reqMock.params = {id: '5523cea79294d58a8e06c3bf'};
 
-    resMock.send = function(object) {
-      expect(object).to.have.property('name');
-      expect(object).to.have.property('price');
-      expect(object.name).to.equal('水溶C100功能饮料');
-      expect(object.specification).to.equal('100ml');
+      resMock.send = function(object) {
+        expect(object).to.have.property('name');
+        expect(object).to.have.property('price');
+        expect(object.name).to.equal('水溶C100功能饮料');
+        expect(object.specification).to.equal('100ml');
 
-      done();
-    };
-    itemController.getItemById(reqMock, resMock);
+        done();
+      };
+      itemController.getItemById(reqMock, resMock);
+    });
   });
 
-  it('should update item', function(done) {
+  describe('send update status', function() {
+    it('should update item', function(done) {
 
-    reqMock.params = {id: '5523cea79294d58a8e06c3bf'};
-    reqMock.body = {inventory: 50};
+      reqMock.params = {id: '5523cea79294d58a8e06c3bf'};
+      reqMock.body = {inventory: 50};
 
-    resMock.send = function(status) {
-      expect(status).to.equal('inventory decrease successful');
-      done();
-    };
-    itemController.updateItem(reqMock, resMock);
+      resMock.send = function(status) {
+        expect(status).to.equal('inventory decrease successful');
+        done();
+      };
+      itemController.updateItem(reqMock, resMock);
+    });
   });
 
-  it('should get correct items', function(done) {
+  describe('get items', function() {
+    it('should get correct items', function(done) {
 
-    reqMock.query = {cartItems: [ { _id: '551cc20e47a654d14a280e9d',
-      __v: '0',
-      item:
-      { _id: '5523cea79294d58a8e06c3c1',
-        name: '2015欧美大牌春装新款 修身显瘦镂空蕾丝连衣裙中长款 夏短袖长裙',
-        unit: '件',
-        price: '368',
-        image: 'image/14.jpg',
-        inventory: '56',
-        description: '这是......',
-        category: '5523bc489294d58a8e06c38f',
-        specification: '100ml',
-        isRecommend: 'true' },
-      number: '4' } ]};
+      reqMock.query = {cartItems: [ { _id: '551cc20e47a654d14a280e9d',
+        __v: '0',
+        item:
+        { _id: '5523cea79294d58a8e06c3c1',
+          name: '2015欧美大牌春装新款 修身显瘦镂空蕾丝连衣裙中长款 夏短袖长裙',
+          unit: '件',
+          price: '368',
+          image: 'image/14.jpg',
+          inventory: '56',
+          description: '这是......',
+          category: '5523bc489294d58a8e06c38f',
+          specification: '100ml',
+          isRecommend: 'true' },
+        number: '4' } ]};
 
-    resMock.send = function(object) {
-      expect(object[0]).to.have.property('name');
-      expect(object.length).to.equal(1);
-      expect(object[0].name).to.equal('2015欧美大牌春装新款 修身显瘦镂空蕾丝连衣裙中长款 夏短袖长裙');
+      resMock.send = function(object) {
+        expect(object[0]).to.have.property('name');
+        expect(object.length).to.equal(1);
+        expect(object[0].name).to.equal('2015欧美大牌春装新款 修身显瘦镂空蕾丝连衣裙中长款 夏短袖长裙');
 
-      done();
-    };
-    itemController.getItems(reqMock, resMock);
+        done();
+      };
+      itemController.getItems(reqMock, resMock);
+    });
   });
 
 });
