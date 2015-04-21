370b93e212da251fc0a3247f198d9be641d87702
hawang <654296217@qq.com>
2015-04-21 12:29:12 +0800

    modify the process of error

--- app.js
+++ app.js
@@ -64,12 +64,13 @@ app.use(function (err, req, res, next) {
       status: err.status || 500,
       massage: err.message
     });
-  }
+  }else{
 
-  res.render('error', {
-    message: err.message,
-    error: err
-  });
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  }
 
   next();
 });
