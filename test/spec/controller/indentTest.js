261b81df490ab59fb12ea7f6ba05ec96b9f5414b
yfwang <yufei930101@gmail.com>
2015-04-21 10:57:41 +0800

    add err test

--- test/spec/controller/indentTest.js
+++ test/spec/controller/indentTest.js
@@ -1,45 +1,76 @@
 'use strict';
 
+var indentController = require('../../../controller/indentController');
+
 describe('GET api/indent', function() {
 
   var resMock = {};
   var reqMock = {};
   var next;
 
-  var indentController = require('../../../controller/indentController');
-
-  it('should get total and indent', function(done) {
-
-    resMock.send = function(object) {
-
-      expect(object).to.have.property('indent');
-      expect(object).to.have.property('total');
-      expect(object.total).to.equal('3334.50');
-
-      done();
-    };
-
-    indentController.getIndent(reqMock, resMock, next);
-  });
-
-  it('should render indent page', function(done) {
-    resMock.render = function(view, object) {
-
-      expect(view).to.equal('indent');
-      expect(object).to.have.property('cartItems');
-      expect(object.cartItems[2].item.shortName).to.equal('2015欧美大牌春装...');
-      expect(object).to.have.property('indent');
-      expect(object.total).to.equal('3334.50');
-      expect(object.shortedCartItemName).to.equal('Nestle雀巢(三合一)速溶咖啡180ml 罐装');
-
-      done();
-    };
-
-    indentController.renderIndentPage(reqMock, resMock, next);
-  });
-
   afterEach(function() {
 
     reloadDatabase();
+  });
+
+  describe('send indent', function() {
+    it('should get total and indent', function(done) {
+
+      resMock.send = function(object) {
+
+        expect(object).to.have.property('indent');
+        expect(object).to.have.property('total');
+        expect(object.total).to.equal('3334.50');
+
+        done();
+      };
+
+      indentController.getIndent(reqMock, resMock, next);
+    });
+
+    it('shoule go into next(err)', function() {
+
+      var getIndentById =  sinon.stub();
+      var next = sinon.spy();
+
+      resMock.send = function() {
+        expect(getIndentById).calledOnce;
+        expect(next).calledOnce;
+      };
+
+      indentController.getIndent(reqMock, resMock, next);
+    });
+  });
+
+
+  describe('rener indent', function() {
+    it('should render indent page', function(done) {
+      resMock.render = function(view, object) {
+
+        expect(view).to.equal('indent');
+        expect(object).to.have.property('cartItems');
+        expect(object.cartItems[2].item.shortName).to.equal('2015欧美大牌春装...');
+        expect(object).to.have.property('indent');
+        expect(object.total).to.equal('3334.50');
+        expect(object.shortedCartItemName).to.equal('Nestle雀巢(三合一)速溶咖啡180ml 罐装');
+
+        done();
+      };
+
+      indentController.renderIndentPage(reqMock, resMock, next);
+    });
+
+    it('should go into next(err) when render', function() {
+
+      var getIndentById =  sinon.stub();
+      var next = sinon.spy();
+
+      resMock.render = function() {
+        expect(next).calledOnce;
+        expect(getIndentById).calledOnce;
+      };
+
+      indentController.renderIndentPage(reqMock, resMock, next);
+    });
   });
 });
